58ef9a0b7c9200f583b69b68027ab42d5b25d8f5
Reidar <reidjoss@gmail.com>
2026-06-02 23:53:11 +0200

    test: exclude playwright specs from vitest

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,6 +6,7 @@ export default defineConfig({
     environment: 'jsdom',
     setupFiles: ['./tests/setup.ts'],
     globals: true,
+    exclude: ['tests/e2e/**', 'node_modules/**', '.next/**'],
   },
   resolve: {
     alias: {
